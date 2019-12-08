7153fc2fe67918565a825d2f6d37797d306433f2
Felixx26 <felixxrafaell@gmail.com>
2019-12-08 08:26:10 -0400

    Agregar archivos de proyecto.

---
+++ ClubDeSocios/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClubDeSocios.Startup))]
+namespace ClubDeSocios
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
